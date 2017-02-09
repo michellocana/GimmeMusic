de5b3d812141943942e8c44a89adaee125cce122
Michell Ocaña <ocana.1997@hotmail.com>
2017-02-08 22:29:59 -0200

    - Getting spotify uri with artist info - Storing artists info in localStorage

--- src/js/controller/appCtrl.js
+++ src/js/controller/appCtrl.js
@@ -5,7 +5,8 @@ angular.module('GimmeMusic.controller.appCtrl', [])
 	$scope.img = document.querySelector('img');
 	$scope.text = document.querySelector('.artist-name');
 
-	$scope.isAnimating = false;
+	$scope.canClick = false;
+
 
 	$scope.buttonPhrases = [
 		'It will rock!',
@@ -16,103 +17,122 @@ angular.module('GimmeMusic.controller.appCtrl', [])
 		'Oh yeh!',
 	];
 
-	$scope.getArtist = function(){
-		if (!$scope.isAnimating) {
-			$scope.isAnimating = !$scope.isAnimating;
-
-			ArtistFactory.getRandomArtist().then(function(result){
-				var storage = firebase.storage();
-				var pathReference = storage.ref('artists/' + result.image);
-
-				pathReference.getDownloadURL().then(function(url) {
-					$scope.newArtist = result;
-					var img = new Image();
-
-					$scope.newArtist.image = url;
-
-					img.src = url;
-
-					img.onload = function(){
-						$scope.changeLabel($scope.newArtist);
-					};
-				})
+	$scope.init = function(){
+		ArtistFactory.getArtists().then(function(result){
+			var artists = result.map(function(artist){
+				return {
+					id: artist.id,
+					name: artist.name
+				}
 			});
+
+			artists = JSON.stringify(artists);
+
+			localStorage.setItem('GimmeMusic-artists', artists);
+
+			$scope.canClick = true;
+		});
+	};
+
+	$scope.getArtist = function(){
+		if ($scope.canClick) {
+			$scope.canClick = !$scope.canClick;
+
+			ArtistFactory.getRandomArtist()
+				.then($scope.handleArtist)
+				.then($scope.changeLabel)
 		}
+	};
+
+	$scope.handleArtist = function(result){
+		return new Promise(function(resolve, reject){
+			$scope.newArtist = result;
+			var img = new Image();
+
+			$scope.newArtist.image = result.image;
+
+			img.src = result.image;
+
+			img.onload = function(){
+				resolve($scope.newArtist)
+			};
+		})
+	};
+
+	$scope.toggleAnimationState = function(){
+		$scope.canClick = !$scope.canClick
 	};
 
 	$scope.changeLabel = function(data){
-		var randomKey = ~~(Math.random() * ($scope.buttonPhrases.length));
-		var newLabel = $scope.buttonPhrases[randomKey];
-		$scope.buttonLabel = newLabel;
-
 		if($scope.artist){
-			$scope.blinkImg(data);
+			// Animation image change
+			$scope.blinkImg(data).then($scope.toggleAnimationState);
 		}else{
 			$scope.$apply(function(){
 				$scope.artist = data;
+				$scope.toggleAnimationState();
 			});
 		}
-
-		$timeout(function(){
-			$scope.isAnimating = !$scope.isAnimating;
-		}, 900);
 	};
 
 	$scope.blinkImg = function(data){
-		var 
-			duration = 1000, // ms
-			startTime = new Date().getTime(),
-			timeEllapsed = 0,
-			updatedData = false,
-			getAnimationProgress = function(ellapsed, duration){
-				return ellapsed  < duration / 2 ? 
-					   1 - ellapsed  / (duration / 2) : 
-					   (ellapsed  / (duration / 2)) - 1;
-			}
-		;
+		return new Promise(function(resolve, reject){
+			var 
+				duration = 1000, // ms
+				startTime = new Date().getTime(),
+				timeEllapsed = 0,
+				updatedData = false,
+				getAnimationProgress = function(ellapsed, duration){
+					return ellapsed  < duration / 2 ? 
+						   1 - ellapsed  / (duration / 2) : 
+						   (ellapsed  / (duration / 2)) - 1;
+				}
+			;
 
-		$scope.img.style.transition = 'none';
+			$scope.img.style.transition = 'none';
 
-		$scope.icons = document.querySelectorAll('.artist-link-icon');
+			$scope.icons = document.querySelectorAll('.artist-link-icon');
 
-		$scope.icons.forEach(function(icon){
-			icon.style.width = '5rem';
-			icon.style.height = '5rem';
-			icon.style.animation = 'none';
-		});
+			$scope.icons.forEach(function(icon){
+				icon.style.width = '5rem';
+				icon.style.height = '5rem';
+				icon.style.animation = 'none';
+			});
 
-		var frame = function(){
-			timeEllapsed = new Date().getTime() - startTime;
+			var frame = function(){
+				timeEllapsed = new Date().getTime() - startTime;
 
-			if (!updatedData && timeEllapsed > duration / 2) {
-				updatedData = !updatedData;
+				if (!updatedData && timeEllapsed > duration / 2) {
+					updatedData = !updatedData;
 
-				$scope.$apply(function(){
-					$scope.artist = data;
+					$scope.$apply(function(){
+						$scope.artist = data;
+					});
+				}
+
+				var opacity = getAnimationProgress(timeEllapsed, duration);
+
+				// Concatenating all nodes that will be animated into an array
+				var elems = Array.prototype.map.call($scope.icons, function(icon){
+					return icon;
 				});
-			}
 
-			var opacity = getAnimationProgress(timeEllapsed, duration);
+				elems.push($scope.img);
+				elems.push($scope.text);
 
-			// Concatenating all nodes that will be animated into an array
-			var elems = Array.prototype.map.call($scope.icons, function(icon){
-				return icon;
-			});
+				elems.forEach(function(icon){
+					icon.style.opacity = opacity;
+				});
 
-			elems.push($scope.img);
-			elems.push($scope.text);
+				if(timeEllapsed <= duration){
+					requestAnimationFrame(frame);
+				}else{
+					cancelAnimationFrame(frame);
+					resolve();
+				}
+			};
 
-			elems.forEach(function(icon){
-				icon.style.opacity = opacity;
-			});
-
-			if(timeEllapsed <= duration){
-				requestAnimationFrame(frame);
-			}else{
-				cancelAnimationFrame(frame);
-			}
-		};
-
-		requestAnimationFrame(frame);
+			requestAnimationFrame(frame);
+		});
 	};
 }])
--- src/js/factory/artist.js
+++ src/js/factory/artist.js
@@ -1,33 +1,65 @@
 angular.module('GimmeMusic.factory.artist', [])
 
 .factory('ArtistFactory', function() {
-	var ref = firebase.database().ref("artists");
+	var 
+		_getArtists = function(){
+			var ref = firebase.database().ref("artists");
 
-	var _getArtists = function(){
-		return new Promise(function(resolve, reject){
-			ref.on("value", function(snapshot){
-				var artists = snapshot.val();
+			return new Promise(function(resolve, reject){
+				ref.on("value", function(snapshot){
+					var artists = snapshot.val();
 
-				artists = Object.keys(artists).map(function(objectKey, index) {
-					return artists[objectKey];
+					artists = Object.keys(artists).map(function(objectKey, index) {
+						var artist = artists[objectKey];
+							artist.id = objectKey;
+
+						return artist;
+					});
+
+					resolve(artists);
+				}, function (errorObject) {
+					reject("The read failed: " + errorObject.code);
 				});
-
-				resolve(artists);
-			}, function (errorObject) {
-				reject("The read failed: " + errorObject.code);
 			});
-		});
-	};
+		},
 
-	var _getRandomArtist = function(){
-		return new Promise(function(resolve, reject){
-			_getArtists().then(function(res){
-				var key = ~~(Math.random() * (res.length));
+		_getRandomArtist = function(){
+			return new Promise(function(resolve, reject){
+				var artists, artist, key, ref;
 
-				resolve(res[key])
+				artists = localStorage.getItem('GimmeMusic-artists');
+				artists = JSON.parse(artists);
+
+				key = ~~(Math.random() * (artists.length));
+
+				artist = artists[key];
+
+				ref = firebase.database().ref('artists/' + artist.id);
+
+				ref.on('value', function(snapshot){
+					artist = snapshot.val();
+
+					_getStorageImage(artist.image).then(function(res){
+						artist.image = res;
+						artist.links = [
+							{
+								type: 'spotify',
+								url: artist.spotify.uri
+							}
+						]
+
+						resolve(artist);
+					});
+				}, function (errorObject) {
+					console.log(errorObject);
+				})
 			});
-		});
-	}
+		},
+
+		_getStorageImage = function(imageName){
+			return firebase.storage().ref('artists/' + imageName).getDownloadURL();
+		}
+	;
 
 	return {
 		getArtists: 		_getArtists,
