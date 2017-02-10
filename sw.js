2a7c96d9cd190bddeaf81867a0542234d5c9fefe
Michell Ocaña <ocana.1997@hotmail.com>
2017-02-09 23:30:49 -0200

    - Base paths on cache registration changed to relative paths.

--- sw.js
+++ sw.js
@@ -3,25 +3,25 @@ self.addEventListener('install', event => {
 		caches.open('static-v1')
 			.then(cache => cache.addAll([
 				// Static content
-				'/',
-				// '/index.html',
+				'./',
+				// './index.html',
 
 				// Images
-				'/dist/img/icon-4x.png',
-				'/dist/img/img.svg',
+				'./dist/img/icon-4x.png',
+				'./dist/img/img.svg',
 
 				// Stylesheets
-				'/dist/css/app.min.css',
+				'./dist/css/app.min.css',
 
 				// Fonts
 				'https://fonts.googleapis.com/css?family=Comfortaa',
 
 				// Javascripts
-				'/src/js/angular.min.js',
-				'/src/js/factory/artist.js',
-				'/src/js/controller/appCtrl.js',
-				'/src/js/app.js',
-				'/src/js/firebase.js'
+				'./src/js/angular.min.js',
+				'./src/js/factory/artist.js',
+				'./src/js/controller/appCtrl.js',
+				'./src/js/app.js',
+				'./src/js/firebase.js'
 			]))
 			.catch(err => {
 				alert(err);
@@ -37,31 +37,6 @@ self.addEventListener('fetch', event => {
 			.then(response => response || fetch(event.request))
 			.catch((err) => {
 				console.log(err);
-				// console.log(event.request);
-				// cache.match('/');
 			})
 	)
-	// const url = new URL(event.request.url);
-
-	// console.log(url);
-
-	// if(url.pathname.endsWidth('.jpg')){
-	// 	event.respondWith(
-	// 		fetch('/dist/img/icon-4x.png')
-	// 	)
-	// }else{
-	// 	event.respondWith(
-	// 		fetch('/index.html')
-	// 	)
-	// }
-
-	// event.respondWith(
-	// 	fetch(event.request).then(response => {
-	// 		if(response === ''){
-	// 			return fetch('/index.html');
-	// 		}else{
-	// 			return fetch('/dist/img/icon-4x.png');
-	// 		}
-	// 	})
-	// );
 });
